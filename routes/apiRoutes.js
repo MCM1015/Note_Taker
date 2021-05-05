4b7fdbddb138dc47535a12663f3d2fe5515829cc
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-05-05 12:53:56 -0400

    updated functioning code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,19 +7,34 @@ module.exports = (app) => {
 
     app.post('/api/notes', (req, res) => {
         console.log(notes);
+
         let newNote = {
             title: req.body.title,
             text: req.body.text,
         }
-        newNote.id = notes[notes.length - 1].id + 1;
 
+        newNote.id = notes[notes.length-1].id + 1;
         notes.push(newNote);
         res.json(true);
         console.log(notes);
+        
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
             err ? console.error(err) : console.log('Success!')
         );
 
     });
+
+    // app.delete(`/api/notes/${notes.length}`, (req, res) => {
+    //     for(i=0; i<notes.length; i++) {
+    //     notes.splice(i, 1);
+    //     }
+    //     console.log(notes);
+    //     for(i=0; i<notes.length; i++) {
+    //         notes[i].id = i;
+    //     }
+    //     fs.writeFile('./db/db.json', JSON.stringify(notes), (err) =>
+    //         err ? console.error(err) : console.log('Success!')
+    //     );
+    // });
 
 }
